04722168c41c6cf2f728b857760b97c7a9fb52ff
onias <onias@noxxonsat.com.br>
2023-07-22 21:24:54 -0300

    .

--- src/main/java/com/ccp/jn/web/spring/application/SpringApplicationStarter.java
+++ src/main/java/com/ccp/jn/web/spring/application/SpringApplicationStarter.java
@@ -1,15 +1,17 @@
-package com.ccp.jn.web.spring.controller.contactus;
+package com.ccp.jn.web.spring.application;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import com.ccp.dependency.injection.CcpDependencyInjection;
-import com.ccp.implementations.cache.Cache;
+import com.ccp.implementations.cache.gcp.memcache.Cache;
 import com.ccp.implementations.db.crud.elasticsearch.Crud;
 import com.ccp.implementations.db.utils.elasticsearch.DbUtils;
-import com.ccp.implementations.file.bucket.FileBucket;
+import com.ccp.implementations.file.bucket.gcp.FileBucket;
+import com.ccp.implementations.http.apache.mime.Http;
 import com.ccp.implementations.mensageria.sender.gcp.pubsub.MensageriaSender;
 import com.ccp.implementations.password.mindrot.Password;
+import com.jn.commons.JnBusinessEntity;
 
 @SpringBootApplication
 public class SpringApplicationStarter {
@@ -23,8 +25,10 @@ public class SpringApplicationStarter {
 				new Password(),
 				new DbUtils(),
 				new Cache(),
+				new Http(),
 				new Crud()
 		);
+		JnBusinessEntity.loadEntitiesMetadata();
 		
 		SpringApplication.run(SpringApplicationStarter.class, args);
 	}
