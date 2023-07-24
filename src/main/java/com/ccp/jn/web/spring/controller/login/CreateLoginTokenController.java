fe27c0236d52dc83adbea677aac6c6d60f8affe3
onias <onias@noxxonsat.com.br>
2023-07-24 07:34:59 -0300

    .

--- src/main/java/com/ccp/jn/web/spring/controller/login/CreateLoginTokenController.java
+++ src/main/java/com/ccp/jn/web/spring/controller/login/CreateLoginTokenController.java
@@ -2,20 +2,21 @@ package com.ccp.jn.web.spring.controller.login;
 
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.ccp.dependency.injection.CcpDependencyInjection;
-import com.ccp.jn.sync.login.controller.SaveLoginRequest;
+import com.ccp.jn.sync.login.controller.CreateLoginToken;
 
 @CrossOrigin
 @RestController
-@RequestMapping(value = "/login/{email}/token", method = RequestMethod.POST)
-public class SaveLoginRequestController {
+@RequestMapping("/login/{email}/token")
+public class CreateLoginTokenController {
 	
-	private SaveLoginRequest injected = CcpDependencyInjection.getInjected(SaveLoginRequest.class);
+	private CreateLoginToken injected = CcpDependencyInjection.getInjected(CreateLoginToken.class);
 
+	@PostMapping
 	public void execute(@PathVariable("email") String email) {
 		this.injected.execute(email);
 	}
