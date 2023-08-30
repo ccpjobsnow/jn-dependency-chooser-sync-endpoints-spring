64d935d7320c4dadf26c48996a94724297e4a414
onias <onias@noxxonsat.com.br>
2023-08-30 16:07:23 -0300

    mensagem qualquer

--- src/main/java/com/ccp/jn/web/spring/controller/login/RequestTokenAgainController.java
+++ src/main/java/com/ccp/jn/web/spring/controller/login/RequestTokenAgainController.java
@@ -3,9 +3,9 @@ package com.ccp.jn.web.spring.controller.login;
 import java.util.Map;
 
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.ccp.decorators.CcpMapDecorator;
@@ -13,11 +13,12 @@ import com.ccp.jn.sync.login.controller.RequestTokenAgain;
 
 @CrossOrigin
 @RestController
-@RequestMapping(value = "/login/{email}/token/request", method = RequestMethod.PATCH)
+@RequestMapping(value = "/login/{email}/token/request")
 public class RequestTokenAgainController {
 
 	private final RequestTokenAgain injected = new RequestTokenAgain();
 	
+	@PatchMapping
 	public Map<String, Object> execute(@PathVariable("email") String email) {
 		CcpMapDecorator execute = this.injected.execute(email);
 		return execute.content;
--- src/main/java/com/ccp/jn/web/spring/controller/login/UnlockTokenController.java
+++ src/main/java/com/ccp/jn/web/spring/controller/login/UnlockTokenController.java
@@ -3,9 +3,10 @@ package com.ccp.jn.web.spring.controller.login;
 import java.util.Map;
 
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.ccp.decorators.CcpMapDecorator;
@@ -13,13 +14,14 @@ import com.ccp.jn.sync.login.controller.UnlockToken;
 
 @CrossOrigin
 @RestController
-@RequestMapping(value = "/login/{email}/token/lock", method = RequestMethod.DELETE)
+@RequestMapping(value = "/login/{email}/token/lock")
 public class UnlockTokenController {
 
 	private final UnlockToken injected = new UnlockToken();
-
-	public Map<String, Object> execute(@PathVariable("email") String email) {
-		CcpMapDecorator execute = this.injected.execute(email);
+	
+	@DeleteMapping
+	public Map<String, Object> execute(@PathVariable("email") String email, @RequestBody Map<String, Object> body) {
+		CcpMapDecorator execute = this.injected.execute(new CcpMapDecorator(body).put("email", email));
 		return execute.content;
 	}
 	
