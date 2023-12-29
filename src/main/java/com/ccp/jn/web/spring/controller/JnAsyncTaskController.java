965951f12594b38854e21db6b94cc1fb8eb6a3d7
ccpjobsnow <69213361+ccpjobsnow@users.noreply.github.com>
2023-12-29 00:07:36 -0300

    ajustes no projeto async

--- src/main/java/com/ccp/jn/web/spring/controller/JnAsyncTaskController.java
+++ src/main/java/com/ccp/jn/web/spring/controller/JnAsyncTaskController.java
@@ -11,23 +11,31 @@ import org.springframework.web.bind.annotation.RestController;
 import com.ccp.decorators.CcpMapDecorator;
 import com.ccp.jn.sync.service.JnSyncAsyncTaskService;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
+
 @CrossOrigin
 @RestController
 @RequestMapping(value = "/async/task")
+@Tag(name = "AsyncTask", description = "Tarefas assíncronas, aquelas que são executadas em segundo plano")
 public class JnAsyncTaskController {
-	
+
 	private final JnSyncAsyncTaskService injected = new JnSyncAsyncTaskService();
-	
-	@GetMapping
-	
-	public String oi() {
-		return "olá mundo";
-	}
-	
+
+	@Operation(summary = "Obter tarefa status da tarefa assíncrona pelo id")
 	@GetMapping("/{asyncTaskId}")
-	public Map<String, Object> execute(@PathVariable("asyncTaskId") String asyncTaskId){
+	@ApiResponses(value = { @ApiResponse(content = {
+			@Content(mediaType = "application/json", schema = @Schema(example = "{}")) }, responseCode = "200", description = "Status: 'Tarefa assíncrona encontrada'"),
+			@ApiResponse(content = {
+					@Content(mediaType = "application/json", schema = @Schema(example = "{}")) }, responseCode = "404", description = "Status: 'Tarefa assíncrona não encontrada'"
+							), })
+	public Map<String, Object> getAsyncTaskStatusById(@PathVariable("asyncTaskId") String asyncTaskId) {
 		CcpMapDecorator execute = this.injected.apply(asyncTaskId);
 		return execute.content;
 	}
-	
+
 }
--- src/main/java/com/ccp/jn/web/spring/controller/JnSupportController.java
+++ src/main/java/com/ccp/jn/web/spring/controller/JnSupportController.java
@@ -12,6 +12,8 @@ import com.ccp.decorators.CcpMapDecorator;
 import com.ccp.jn.sync.service.JnSyncSupportService;
 
 import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.tags.Tag;
@@ -27,12 +29,12 @@ public class JnSupportController {
 			+ "<br/><br/>Para que serve? Para enviar por e-mail ao usuário, a senha temporária que se corretamente digitada, lhe desbloqueará o token."
 			)
 	 @ApiResponses(value = {
-			 	@ApiResponse(responseCode = "200", description = "Status: 'Senha temporária enviada com sucesso' <br/><br/> Quando ocorre? Quando o operador tem permissão para atender a solicitação, quando existe uma solicitação de desbloqueio de token, quando o token está de fato bloqueado e quando esta solicitação ainda não foi respondida."),
-		        @ApiResponse(responseCode = "400", description = "Status: 'Email inválido' <br/><br/> Quando ocorre? Quando a url path recebe um conjunto de caracteres que não representa um e-mail válido.<br/><br/>Qual comportamento esperado do front end? Apresentar erro genérico de sistema para o usuário."),
-		        @ApiResponse(responseCode = "401", description = "Status: 'Operador sem permissão para atender esta solicitação' <br/><br/> Quando ocorre? Quando o operador não está cadastrado na tabela correta para atender este tipo de solicitação."),
-		        @ApiResponse(responseCode = "404", description = "Status: 'Token não encontrado' <br/><br/> Quando ocorre? Quando o token do usuário não é encontrado."),
-		        @ApiResponse(responseCode = "409", description = "Status: 'Solicitação já respondida' <br/><br/> Quando ocorre? Quando a solicitação consta na tabela de solicitações (deste tipo) respondidas."),
-		        @ApiResponse(responseCode = "422", description = "Status: 'Solicitação inexistente' <br/><br/> Quando ocorre? Quando a solicitação não existe."),
+			 	@ApiResponse(content = {@Content(mediaType = "application/json",schema = @Schema(example = "{'asyncTaskId':'ticketDaFilaSha1EmBase64'}"))}, responseCode = "200", description = "Status: 'Senha temporária enviada com sucesso' <br/><br/> Quando ocorre? Quando o operador tem permissão para atender a solicitação, quando existe uma solicitação de desbloqueio de token, quando o token está de fato bloqueado e quando esta solicitação ainda não foi respondida."),
+		        @ApiResponse(content = {@Content(mediaType = "application/json",schema = @Schema(example = "{}"))}, responseCode = "400", description = "Status: 'Email inválido' <br/><br/> Quando ocorre? Quando a url path recebe um conjunto de caracteres que não representa um e-mail válido.<br/><br/>Qual comportamento esperado do front end? Apresentar erro genérico de sistema para o usuário."),
+		        @ApiResponse(content = {@Content(mediaType = "application/json",schema = @Schema(example = "{}"))}, responseCode = "401", description = "Status: 'Operador sem permissão para atender esta solicitação' <br/><br/> Quando ocorre? Quando o operador não está cadastrado na tabela correta para atender este tipo de solicitação."),
+		        @ApiResponse(content = {@Content(mediaType = "application/json",schema = @Schema(example = "{}"))}, responseCode = "404", description = "Status: 'Token não encontrado' <br/><br/> Quando ocorre? Quando o token do usuário não é encontrado."),
+		        @ApiResponse(content = {@Content(mediaType = "application/json",schema = @Schema(example = "{}"))}, responseCode = "409", description = "Status: 'Solicitação já respondida' <br/><br/> Quando ocorre? Quando a solicitação consta na tabela de solicitações (deste tipo) respondidas."),
+		        @ApiResponse(content = {@Content(mediaType = "application/json",schema = @Schema(example = "{}"))}, responseCode = "422", description = "Status: 'Solicitação inexistente' <br/><br/> Quando ocorre? Quando a solicitação não existe."),
 	 })	
 	@PostMapping("/token/{email}/unlock")
 	public Map<String, Object> answerUnlockTokenRequest(@PathVariable("chatId") String chatId, @PathVariable("email") String email) {
@@ -45,12 +47,12 @@ public class JnSupportController {
 			+ "<br/><br/>Para que serve? Para que o suporte JobsNow possa enviar por e-mail ao usuário, o token caso ele não tenha recebido, geralmente pelo fornecedor de API de e-mail do JobsNow ser bloqueado pelo provedor de quem está recebendo e-mail."
 			)
 	 @ApiResponses(value = {
-			 	@ApiResponse(responseCode = "200", description = "Status: 'Senha temporária enviada com sucesso' <br/><br/> Quando ocorre? Quando o operador tem permissão para atender a solicitação, quando existe uma solicitação de desbloqueio de token, quando o token está de fato bloqueado e quando esta solicitação ainda não foi respondida."),
-		        @ApiResponse(responseCode = "400", description = "Status: 'Email inválido' <br/><br/> Quando ocorre? Quando a url path recebe um conjunto de caracteres que não representa um e-mail válido.<br/><br/>Qual comportamento esperado do front end? Apresentar erro genérico de sistema para o usuário."),
-		        @ApiResponse(responseCode = "401", description = "Status: 'Operador sem permissão para atender esta solicitação' <br/><br/> Quando ocorre? Quando o operador não está cadastrado na tabela correta para atender este tipo de solicitação."),
-		        @ApiResponse(responseCode = "404", description = "Status: 'Token não encontrado' <br/><br/> Quando ocorre? Quando o token do usuário não é encontrado."),
-		        @ApiResponse(responseCode = "409", description = "Status: 'Solicitação já respondida' <br/><br/> Quando ocorre? Quando a solicitação consta na tabela de solicitações (deste tipo) respondidas."),
-		        @ApiResponse(responseCode = "422", description = "Status: 'Solicitação inexistente' <br/><br/> Quando ocorre? Quando a solicitação não existe."),
+			 	@ApiResponse(content = {@Content(mediaType = "application/json",schema = @Schema(example = "{'asyncTaskId':'ticketDaFilaSha1EmBase64'}"))}, responseCode = "200", description = "Status: 'Senha temporária enviada com sucesso' <br/><br/> Quando ocorre? Quando o operador tem permissão para atender a solicitação, quando existe uma solicitação de desbloqueio de token, quando o token está de fato bloqueado e quando esta solicitação ainda não foi respondida."),
+		        @ApiResponse(content = {@Content(mediaType = "application/json",schema = @Schema(example = "{}"))}, responseCode = "400", description = "Status: 'Email inválido' <br/><br/> Quando ocorre? Quando a url path recebe um conjunto de caracteres que não representa um e-mail válido.<br/><br/>Qual comportamento esperado do front end? Apresentar erro genérico de sistema para o usuário."),
+		        @ApiResponse(content = {@Content(mediaType = "application/json",schema = @Schema(example = "{}"))}, responseCode = "401", description = "Status: 'Operador sem permissão para atender esta solicitação' <br/><br/> Quando ocorre? Quando o operador não está cadastrado na tabela correta para atender este tipo de solicitação."),
+		        @ApiResponse(content = {@Content(mediaType = "application/json",schema = @Schema(example = "{}"))}, responseCode = "404", description = "Status: 'Token não encontrado' <br/><br/> Quando ocorre? Quando o token do usuário não é encontrado."),
+		        @ApiResponse(content = {@Content(mediaType = "application/json",schema = @Schema(example = "{}"))}, responseCode = "409", description = "Status: 'Solicitação já respondida' <br/><br/> Quando ocorre? Quando a solicitação consta na tabela de solicitações (deste tipo) respondidas."),
+		        @ApiResponse(content = {@Content(mediaType = "application/json",schema = @Schema(example = "{}"))}, responseCode = "422", description = "Status: 'Solicitação inexistente' <br/><br/> Quando ocorre? Quando a solicitação não existe."),
 	 })	
 	@PostMapping("/token/{email}/resending")
 	public Map<String, Object> resentTokenToTheUser(@PathVariable("chatId") String chatId, @PathVariable("email") String email) {
